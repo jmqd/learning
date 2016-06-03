787e6f2383661e24e273121e5e506589f71129ad
Jordan McQueen <dojo3@uw.edu>
2016-06-03 13:31:33 -0700

    adding argument parsing to mv_images

--- python/mv_images_slug_rename/mv_images.py
+++ python/mv_images_slug_rename/mv_images.py
@@ -1,9 +1,13 @@
-import os, re
+import os, re, argparse
 from shutil import copyfile
 
+#argument parsing :::
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--destination", type=str, default='new/')
+parser.add_argument("-s", "--source", type=str, default='old/')
+args = parser.parse_args()
 
-prev = "prev/"
-new = "new/"
+#regular expression object building :::
 regex = re.compile('[^a-zA-Z\-]') #looking at complement of any alphabetical character, or a hyphen
 
 def slug(string):
@@ -13,8 +17,8 @@ def slug(string):
     string = re.sub(r"([-]){2,}", "-", string) #solve for that here: remove case of two adjacent hyphens
     return string
 
-for file in os.listdir(prev):
+for file in os.listdir(args.source):
     if file.endswith(".jpg"):
-        src = prev + file
-        dest = new + slug(os.path.splitext(file)[0]) + ".jpg" #splitext -> list of 0:name, 1:extension
+        src = args.source + file
+        dest = args.destination + slug(os.path.splitext(file)[0]) + ".jpg" #splitext: splits name & ext
         copyfile(src, dest)
