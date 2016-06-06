35dd27098ba2716e8586e6328f0bd823743cd421
Jordan McQueen <dojo3@uw.edu>
2016-06-05 23:34:58 -0700

    adding broken start to 5th problem

--- c++/euler/5_smallest_multiple.cpp
+++ c++/euler/5_smallest_multiple.cpp
@@ -1,3 +1,5 @@
+// This doesn't compile. Something is badly wrong with this.
+//
 // What is the smallest positive number that is evenly 
 // divisible by all of the numbers from 1 to 20?
 
@@ -11,28 +13,58 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-int isPrime();
 
 int main()
 {
+    std::vector<int> primeFactorization(int current);
     int begin = 1;
     int end = 10;
-    std::vector<int> prime_factors;
+    std::map<int, int> prime_factorization;
 
-    for (int i = begin; i <= end; ++i)
+
+    for (int current = begin; current <= end; ++current)
     {
-        prime_factors.push_back(i);
+        std::vector<int> prime_factors = primeFactorization(current);
+
+        for (auto kv : prime_factorization)
+        {
+            int count = std::count_if(prime_factors.begin(), prime_factors.end(), [prime_factorization, kv](int i) {return i == prime_factorization.find(kv.first).first; });
+            if (count > kv.second)
+            {
+                prime_factorization[kv.first] = count;
+            }
+        }
+    }
+
+    for (auto& elem : prime_factorization)
+    {
+        std::cout << elem.first << " " << elem.second << "\n";
     }
 }
 
-int isPrime(int num)
+std::vector<int> primeFactorization(int num)
 {
-   for (int i = 1; i * i <= num; ++i)
-   {
-        if (i % num == 0)
+	std::vector<int> values;
+	while (num % 2 == 0)
+    {
+		values.push_back(2);
+        num /= 2;
+    }
+ 
+    for (int divisor = 3; divisor * divisor <= num; divisor += 2)
+    {
+        while (num % divisor == 0)
         {
-            return false;
+			values.push_back(divisor);
+            num /= divisor;
         }
-   }
-   return true;
+    }
+ 
+    // This condition is to handle the case whien n is a prime number
+    // greater than 2
+    if (num > 2)
+	{
+		values.push_back(num);
+	}
+	return values;
 }
