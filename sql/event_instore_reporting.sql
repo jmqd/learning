55d900f90659f7129f2bebe3acb9b64050d9711e
Jordan McQueen <jmcqueen@cardkingdom.com>
2016-06-16 15:36:36 -0700

    adding LPS event; style/formatting

--- sql/event_instore_reporting.sql
+++ sql/event_instore_reporting.sql
@@ -1,12 +1,15 @@
 select
   date(ship_date) as `date`,
   `message_table`.`event_type`,
+  count(*) as `order_count`,
   sum(o.total) as `total`,
   case when payment_type = 'paypal' then sum(o.total) end as `paypal`,
-  case when payment_type = 'credit' then sum(o.total) end as `credit`,
+  case when payment_type = 'credit' then sum(o.total) end as `credit_card`,
   case when payment_type = 'check' then sum(o.total) end as `check`,
   case when payment_type = 'mocc' then sum(o.total) end as `mocc`,
-  case when payment_type not in ('paypal', 'credit', 'mocc', 'check') then sum(o.total) end as `unknown_payment_type`
+  case when payment_type = 'storecredit' then sum(o.total) end as `storecredit`,
+  case when payment_type not in ('paypal', 'credit', 'mocc', 'check') 
+    then sum(o.total) end as `unknown_payment_type`
 from
   orders o
     inner join (
@@ -16,15 +19,20 @@ from
           when message like "%FNM%" then 'FNM'
           when message like "%Legacy%" then 'Legacy'
           when message like "%2DH%" then '2DH'
-          when message like "%Marquee%" then '1K'
+          when message like "%Marquee%" then 'Marquee'
+          when message like "%LPS%" then 'LPS'
             end as `event_type`
       from
         logposts
       where 
         (
-          message like "%FNM%" or message like "%Legacy%" or message like "%2DH%" or message like "%Marquee%"
+          message like "%FNM%" or 
+          message like "%Legacy%" or 
+          message like "%2DH%" or 
+          message like "%Marquee%" or
+          message like "%LPS%"
         )
-        and date(timestamp) >= "2016-04-22"
+        and date(timestamp) between curdate()-interval 1 week and now()
     ) `message_table` on o.id = `message_table`.order_id
 where 
   o.status = 'completed'
