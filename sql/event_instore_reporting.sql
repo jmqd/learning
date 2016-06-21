23a79f85427ae7c0350872d613ab55e6a3d5f4b9
Jordan McQueen <dojo3@uw.edu>
2016-06-21 10:41:15 -0700

    adding null payment type

--- sql/event_instore_reporting.sql
+++ sql/event_instore_reporting.sql
@@ -3,11 +3,13 @@ select
   `message_table`.`event_type`,
   count(*) as `order_count`,
   sum(o.total) as `total`,
-  sum(case when payment_type = 'paypal' then o.total end) as `paypal`,
-  sum(case when payment_type = 'credit' then o.total end) as `credit`,
-  sum(case when payment_type = 'check' then o.total end) as `check`,
-  sum(case when payment_type = 'mocc' then o.total end) as `mocc`
-  sum(case when payment_type = 'storecredit' then o.total end) as `storecredit`
+  sum(case when payment_type = 'paypal' then o.total else 0 end) as `paypal`,
+  sum(case when payment_type = 'credit' then o.total else 0 end) as `credit`,
+  sum(case when payment_type = 'check' then o.total else 0 end) as `check`,
+  sum(case when payment_type = 'mocc' then o.total else 0 end) as `mocc`,
+  sum(case when payment_type = 'storecredit' then o.total else 0 end) as `storecredit`,
+  sum(case when payment_type not in ('paypal', 'credit', 'check', 'mocc', 'storecredit') 
+    then o.total else 0 end) as `null_payment_type`
 from
   orders o
     inner join (
@@ -23,19 +25,20 @@ from
             end as `event_type`
       from
         logposts
-      where 
+      where
         (
-          message like "%FNM%" or 
-          message like "%Legacy%" or 
+          message like "%FNM%" or
+          message like "%Legacy%" or
           message like "%modern%" or
-          message like "%2DH%" or 
+          message like "%2DH%" or
           message like "%Marquee%" or
           message like "%LPS%"
         )
-        and date(timestamp) between curdate()-interval 1 week and now()
+        and date(timestamp) between curdate()-interval 20 week and now()
     ) `message_table` on o.id = `message_table`.order_id
-where 
+where
   o.status = 'completed'
   and o.type = 'sale'
   and o.is_pickup = 1
 group by `event_type`, `date`
+
