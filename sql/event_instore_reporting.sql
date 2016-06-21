963a1ab2758bc2f2285836820ba69534d092a218
Jordan McQueen <jmcqueen@cardkingdom.com>
2016-06-21 12:12:25 -0700

    correcting to query payments table

--- sql/event_instore_reporting.sql
+++ sql/event_instore_reporting.sql
@@ -3,13 +3,10 @@ select
   `message_table`.`event_type`,
   count(*) as `order_count`,
   sum(o.total) as `total`,
-  case when payment_type = 'paypal' then sum(o.total) end as `paypal`,
-  case when payment_type = 'credit' then sum(o.total) end as `credit_card`,
-  case when payment_type = 'check' then sum(o.total) end as `check`,
-  case when payment_type = 'mocc' then sum(o.total) end as `mocc`,
-  case when payment_type = 'storecredit' then sum(o.total) end as `storecredit`,
-  case when payment_type not in ('paypal', 'credit', 'mocc', 'check') 
-    then sum(o.total) end as `unknown_payment_type`
+  sum(case when p.type = 'paypal' then p.amount else 0 end) as `paypal`,
+  sum(case when p.type = 'credit' then p.amount else 0 end) as `credit_card`,
+  sum(case when p.type = 'storecredit' then p.amount else 0 end) as `storecredit`,
+  sum(case when p.type = 'mocc' then p.amount else 0 end) as `mocc`
 from
   orders o
     inner join (
@@ -21,23 +18,25 @@ from
           when message like "%modern%" then 'Modern'
           when message like "%2DH%" then '2DH'
           when message like "%Marquee%" then 'Marquee'
-          when message like "%LPS%" then 'LPS'
+          when (message like "%LPS%" or message like "%Tuesday%") then 'Tuesday'
             end as `event_type`
       from
         logposts
-      where 
+      where
         (
-          message like "%FNM%" or 
-          message like "%Legacy%" or 
+          message like "%FNM%" or
+          message like "%Legacy%" or
           message like "%modern%" or
-          message like "%2DH%" or 
+          message like "%2DH%" or
           message like "%Marquee%" or
           message like "%LPS%"
         )
-        and date(timestamp) between curdate()-interval 1 week and now()
+        and date(timestamp) between curdate() - interval 1 week and now()
     ) `message_table` on o.id = `message_table`.order_id
-where 
+    inner join payments p on o.id = p.`order_id`
+where
   o.status = 'completed'
   and o.type = 'sale'
   and o.is_pickup = 1
 group by `event_type`, `date`
+order by `date`
