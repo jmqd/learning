46acaa9e9b89849ccb3b8cad5a1895d20419e748
Jordan McQueen <dojo3@uw.edu>
2016-07-16 15:57:03 -0700

    finished 4th problem. working, I believe. Nice little divide and conquer

--- python/google_foobar/3_string_cleaning/solution.py
+++ python/google_foobar/3_string_cleaning/solution.py
@@ -1,2 +1,38 @@
+def get_occurences(chunk, word):
+    start = 0
+    starts = []
+    while True:
+        start = chunk.find(word, start) + 1
+        if start > 0:
+            starts.append(start - 1)
+        else:
+            return starts
+
+def remove_slice(chunk, length, begin):
+    chunk = chunk[:begin] + chunk[begin + length:]
+    return chunk
+
+
+def decode_chunk(chunk, word, potentials):
+    occurences = get_occurences(chunk, word)
+    if not occurences:
+        potentials.append(chunk)
+    if occurences:
+        for occurence in occurences:
+            chunk_instance = remove_slice(chunk, len(word), occurence)
+            decode_chunk(chunk_instance, word, potentials)
+
+def rank_potentials(potentials):
+    minimum = min(map(len, potentials))
+    for potential in potentials:
+        if len(potential) != minimum:
+            potentials.remove(potential)
+    potentials.sort()
+    return potentials[0]
+
 def answer(chunk, word):
-    # your code here
+    potentials = []
+    decode_chunk(chunk, word, potentials)
+    return rank_potentials(potentials)
+
+answer(chunk, word)
