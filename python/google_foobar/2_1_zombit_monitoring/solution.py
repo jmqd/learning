2db8454447258f549368f4b1fe66561294dd7345
Jordan McQueen <dojo3@uw.edu>
2016-07-16 14:29:17 -0700

    logic fix

--- python/google_foobar/2_1_zombit_monitoring/solution.py
+++ python/google_foobar/2_1_zombit_monitoring/solution.py
@@ -1,5 +1,7 @@
 from operator import itemgetter
 
+intervals = [[1, 100], [4, 10], [4, 18], [19, 20], [19, 20], [13, 20], [50, 60]]
+
 def sort_intervals_by_start_time(intervals):
     return sorted(intervals, key = itemgetter(0))
 
@@ -8,8 +10,13 @@ def answer(intervals):
     intervals = sort_intervals_by_start_time(intervals)
     prev_interval = None
     for interval in intervals:
-        if prev_interval and prev_interval[1] > interval[0]:
-            interval[0] = prev_interval[1]
+        if prev_interval:
+            if prev_interval[1] > interval[0]:
+                interval[0] = prev_interval[1]
+                if interval[0] > interval[1]:
+                    continue
         time_watched += interval[1] - interval[0]
         prev_interval = interval
     return time_watched
+
+answer(intervals)
