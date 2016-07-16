69a71f057b647ac3ade75743d46e20b28acf577c
Jordan McQueen <dojo3@uw.edu>
2016-07-16 12:48:39 -0700

    building out the rest of it... this should be correct.

--- python/google_foobar/2_name_that_rabbit/solution.py
+++ python/google_foobar/2_name_that_rabbit/solution.py
@@ -1,16 +1,46 @@
 
-names = ['jordan']
-values_to_names = {}
+names = ['jordan', 'kyle', 'kathy', 'brian']
 
-def assign_names_to_values(names)
+def build_value_to_name_dict(names):
+    values_to_names = {}
     for name in names:
-        name_value = 0
-        for letter in name:
-            letter_value = ord(letter) - 96
-            name_value += letter_value
+        name_value = assign_value_to_a_name(name)
+        if name_value in values_to_names:
+            values_to_names[name_value].append(name)
         if name_value not in values_to_names:
+            values_to_names[name_value] = [name]
+
+    return values_to_names
+
+def assign_value_to_a_name(name):
+    name_value = 0
+    for letter in name:
+        # could also make a dict of letters:values and query that
+        letter_value = ord(letter) - 96
+        name_value += letter_value
+    return name_value
+
+
+def decide_tiebreak(names):
+    names.sort(reverse = True)
+    return names
+
+
+def get_final_ordering(vals_to_names):
+    final_ordering = []
+    for val, names in iter(sorted(vals_to_names.iteritems(), reverse = True)):
+        if len(names) > 1:
+            names = decide_tiebreak(names)
+        for name in names:
+            final_ordering.append(name)
+    return final_ordering
+
 
 def answer(names):
+    values_to_names = build_value_to_name_dict(names)
+    final_ordering = get_final_ordering(values_to_names)
+    print final_ordering
+    return final_ordering
 
 
 answer(names)
