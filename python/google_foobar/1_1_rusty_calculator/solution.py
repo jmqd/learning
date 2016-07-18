dd3bcf40b8bb6227135a2784398be4b4d4581d9e
Jordan McQueen <dojo3@uw.edu>
2016-07-17 18:13:03 -0700

    solved another foobar challenge

---
+++ python/google_foobar/1_1_rusty_calculator/solution.py
@@ -0,0 +1,34 @@
+case_1 = '2*4*3+9*3+5'
+
+
+def answer(str):
+    mult = 0
+    plus = 0
+    new_str = ''
+    for char in str:
+        if char == '*':
+            mult += 1
+        if char == '+':
+            plus += 1
+            new_str += '*' * mult
+            mult = 0
+        if char != '*' and char != '+':
+            new_str += char
+    new_str += '+' * plus
+    if mult > 0:
+        new_str += mult * '*'
+    return new_str
+
+def tester():
+    cases = [
+        '2*4*3*2+1+2+3*1+3+1*0',
+        '2+1',
+        '2*2',
+        ]
+    for case in cases:
+        print case
+        print answer(case)
+
+tester()
+
+
