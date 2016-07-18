3ce8bc6709403a598cf26ec3dd2a1d1ed4ba2bea
Jordan McQueen <dojo3@uw.edu>
2016-07-18 01:18:11 -0700

    answered hash it out. easier than anticipated.

--- python/google_foobar/3_hash_it_out/solution.py
+++ python/google_foobar/3_hash_it_out/solution.py
@@ -1,2 +1,8 @@
-answer(digest):
-    # your code here
+def answer(digest):
+    message = [0]
+    for j in xrange(0, len(digest)):
+        for i in xrange(digest[j], 129*255, 256):
+            if (i ^ message[j]) % 129 == 0:
+                message.append((i^message[j])/129)
+                continue
+    return message[1:]
