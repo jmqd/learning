ea3009eaba1ca170f33d169778cfcb352ab3e9db
Jordan McQueen <dojo3@uw.edu>
2016-07-19 00:07:51 -0700

    solved minion interrogation

--- python/google_foobar/3_minion_interrogation/solution.py
+++ python/google_foobar/3_minion_interrogation/solution.py
@@ -1,2 +1,17 @@
-answer(minions):
-    # your code here
+import operator
+
+def answer(minions):
+    answer = []
+    final_answer = []
+    for i in xrange(0, len(minions)):
+        minutes = minions[i][0]
+        numerator = minions[i][1]
+        denominator = minions[i][2]
+        value = (numerator / float(denominator)) / float(minutes)
+        answer.append((i, value))
+    answer.sort(key = operator.itemgetter(0))
+    answer.sort(reverse = True, key = operator.itemgetter(1))
+    for minion in answer:
+        final_answer.append(minion[0])
+    return final_answer
+
