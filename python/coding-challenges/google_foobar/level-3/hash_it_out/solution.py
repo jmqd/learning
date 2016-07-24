b8994fccf1b4b03283f9cd02b45ff21457b7bb22
Jordan McQueen <dojo3@uw.edu>
2016-07-23 21:26:24 -0700

    continue needed to be break; was bothering me for a while, but kept forgetting to fix. :_

--- python/coding-challenges/google_foobar/level-3/hash_it_out/solution.py
+++ python/coding-challenges/google_foobar/level-3/hash_it_out/solution.py
@@ -3,6 +3,7 @@ def answer(digest):
     for j in xrange(0, len(digest)):
         for i in xrange(digest[j], 129*255, 256):
             if (i ^ message[j]) % 129 == 0:
-                message.append((i^message[j])/129)
-                continue
+                message.append((i ^ message[j]) / 129)
+                break
     return message[1:]
+
