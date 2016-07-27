4b17c731b5f1c5c18383973fb0e093767d5eb65d
Jordan McQueen <jmcqueen@cardkingdom.com>
2016-07-27 14:04:31 -0700

    published my api keys... don't bother trying; they've been changed

--- python/pandas/currency_exchange_proof_of_concept.py
+++ python/pandas/currency_exchange_proof_of_concept.py
@@ -1,6 +1,8 @@
 import quandl
 import pandas as pd
 
+# yes, I know I published my API keys... we all type git push origin master
+# too quickly from time to time... needless to say, I have changed my api keys
 quandl.ApiConfig.api_key = 'joPSdv8a2s7kszCLosBz'
 quandl.ApiConfig.api_version = '2015-04-09'
 
