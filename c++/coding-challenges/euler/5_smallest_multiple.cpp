44d06eaafdd7b4a40a8a201eafca70997b4933dd
Jordan McQueen <dojo3@uw.edu>
2016-07-28 04:10:11 -0700

    committing..

--- c++/coding-challenges/euler/5_smallest_multiple.cpp
+++ c++/coding-challenges/euler/5_smallest_multiple.cpp
@@ -1,4 +1,5 @@
 // This doesn't compile. Something is badly wrong with this.
+// This is also utter garbage. ;) I should know better than all of this.
 //
 // What is the smallest positive number that is evenly 
 // divisible by all of the numbers from 1 to 20?
