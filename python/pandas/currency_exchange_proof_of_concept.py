0cdebb533b33a71d56145093cb0f3944651e8b2f
Jordan McQueen <dojo3@uw.edu>
2016-07-28 01:10:28 -0700

    proof of concept for currency conversion idea

--- python/pandas/currency_exchange_proof_of_concept.py
+++ python/pandas/currency_exchange_proof_of_concept.py
@@ -1,9 +1,10 @@
 import quandl
 import pandas as pd
+import secret_file
 
 # yes, I know I published my API keys... we all type git push origin master
 # too quickly from time to time... needless to say, I have changed my api keys
-quandl.ApiConfig.api_key = 'joPSdv8a2s7kszCLosBz'
+quandl.ApiConfig.api_key = secret_file.api_key
 quandl.ApiConfig.api_version = '2015-04-09'
 
 def in_usd(date, amount, currency):
---
+++ python/pandas/scalable/README.md
@@ -0,0 +1,2 @@
+# Currency Converter
+#### A proof-of-concept of a solution to a currency conversion problem involving historical sales data. Inputs: a csv of [row_id, date, currency_code, amount]. Outputs: text to standard output [row_id, conversion, amount_in_usd]
