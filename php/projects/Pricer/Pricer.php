7a6d95786d873788870fb2e954c5ab0804153837
Jordan McQueen <dojo3@uw.edu>
2016-08-05 12:00:13 -0700

    Pricer changes

--- php/projects/Pricer/Pricer.php
+++ php/projects/Pricer/Pricer.php
@@ -1,20 +1,38 @@
 <?php
+/*
+ * Repricer class -- let's aim to include all repricing logic in this class.
+ * @author Jordan McQueen
+ *
+ * Meant to be used in a singleton-ish way.
+ * e.g.
+ *
+ * $pricer = new Pricer();
+ * $products = bar(); // must be magic cards
+ * foreach ($products as $p)
+ * {
+ *      $step = foo();
+ *      $new_price = $pricer->reprice($p, $step);
+ * }
+ *
+ * This class DOES NOT modify the objects it is given, only returns results.
+ */
 
 class Pricer
 {
 
     public function __construct()
     {
-        $config = include('config.php');
-        $this->prices = $config['prices_array'];
-        $this->array_size = count($config['prices_array']);
-        $this->breaks = $config['breaks'];
+        $this->config = include('config.php');
+        $this->breaks = $this->config['breaks'];
         $this->number_of_breaks = count($this->breaks);
+        $this->rarity_floors = $this->config['rarity_floors'];
     }
 
-    public function conform($price, $step = 0)
+
+    public function conform($product, $step = 0)
     {
-        $this->restart();
+        $this->instantiate($product);
+        $price = $product->price;
         $price = round($price, 2);
         if ($price >= $this->prices[$this->array_size - 1])
         {
@@ -26,7 +44,7 @@ class Pricer
                     $this->discrete_increment = $this->breaks[$i]['discrete_increment'];
                     break;
                 }
-                
+
                 // this is the correct break point
                 // iff price > break_price & price > next_break_price ||
                 // break_price == max(break_prices)
@@ -75,7 +93,7 @@ class Pricer
             $closest = null;
             foreach ($this->prices as $index => $price_step)
             {
-                if ($closest === null 
+                if ($closest === null
                     or abs($price - $closest) > abs($price_step - $price))
                 {
                     $closest = $price_step;
@@ -87,31 +105,26 @@ class Pricer
     }
 
 
-    private function restart()
+    private function instantiate($product)
     {
+        $this->prices = $this->config['prices_array'];
         $this->discrete_increment = null;
+        $price_floor_index = array_search(
+            $this->rarity_floors[$product->cfields->rarity],
+            $this->prices);
+        $this->prices = array_slice($this->prices, $price_floor_index);
+        $this->array_size = count($this->prices);
     }
 
 
-    public function suggest($product)
+
+    public function reprice($product, $step)
     {
-        $price = $product->price;
-        $price = $this->conform($price);
-
-        // price suggestion algorithm code here ...
-
-        return $price;
-    }
-
-
-    public function reprice($price, $step)
-    {
-        $this->restart();
-        $price = $this->conform($price, $step);
+        $price = $this->conform($product, $step);
 
         if ($price > $this->prices[$this->array_size - 1]
-            and $step < 0 
-            and $price + $this->discrete_increment * $step 
+            and $step < 0
+            and $price + $this->discrete_increment * $step
             < $this->prices[$this->array_size - 1])
         {
             while ($price > $this->prices[$this->array_size - 1])
@@ -123,7 +136,7 @@ class Pricer
             $price = $this->prices[$this->array_size - 1 + $step];
             return $price;
         }
-        
+
         if ($price > $this->prices[$this->array_size - 1])
         {
             $price += $this->discrete_increment * $step;
@@ -131,7 +144,7 @@ class Pricer
         }
 
         $price_key = array_search($price, $this->prices);
-        
+
         if (($price_key + $step) > $this->array_size - 1)
         {
             $extra_steps = $step - ($this->array_size - 1 - $price_key);
@@ -154,23 +167,28 @@ class Pricer
         {
             return $min + mt_rand() / mt_getrandmax() * ($max - $min);
         }
-        echo "<pre>\n";
+        echo "<pre>";
         echo "<h3>This is the unit test module for Pricer.php.\n"
             . " Shows input of price and step, and resulting output price.\n"
             . " Inputs are randomly generated along ranges of known test cases.</h3>";
 
-        foreach ([1.00, 49.99, 199.99, 5000] as $max)
+        foreach ([0.45, 1.00, 199.99, 5000] as $max)
         {
             for ($i = 0; $i < 5; ++$i)
             {
                 $num = round(rng(0, $max), 2);
                 $step = mt_rand(-4, 4);
-                $result = $this->reprice($num, $step);
-                echo "\n$num going $step steps: $result";
+                $rarities = ['C', 'U', 'R', 'M', 'L', 'S'];
+                $rarity = $rarities[array_rand($rarities)];
+                $cfields = (object)['rarity' => $rarity];
+                $product = (object)['price' => $num, 'cfields' => $cfields];
+                $result = $this->reprice($product, $step);
+                echo "\n$product->price ({$product->cfields->rarity}) going $step steps: $result";
             }
         }
-        echo "\n</pre>";
+        echo "</pre>";
     }
 
 }
 ?>
+
