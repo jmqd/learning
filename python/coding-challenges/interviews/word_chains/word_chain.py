63aaef83f9d6763b83be97e5542293da9b37c77d
Jordan McQueen <dojo3@uw.edu>
2016-08-13 10:06:16 -0700

    word chain problem

---
+++ python/coding-challenges/interviews/word_chains/word_chain.py
@@ -0,0 +1,49 @@
+import Graph, argparse, csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dictionary', default='dictionary.csv')
+args = parser.parse_args()
+
+def build_dictionary(words):
+    dictionary = {}
+    for word in words:
+        dictionary[word] = []
+    while len(words) > 0:
+        word = words.pop()
+        for word_i in words:
+            if is_chainable(word, word_i):
+                dictionary[word].append(word_i)
+                dictionary[word_i].append(word)
+    return dictionary
+
+
+def is_chainable(string_1, string_2):
+    if len(string_1) != len(string_2):
+        return False
+    difference = False
+    for i in range(len(string_1)):
+        if string_1[i] != string_2[i]:
+            if difference:
+                return False
+            difference = True
+    if difference:
+        return True
+    return False
+
+def import_words(filename):
+    words = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            words.append(row[0])
+    return words
+
+words = import_words(args.dictionary)
+dictionary = build_dictionary(words)
+graph = Graph.Graph(dictionary)
+graph.show()
+while True:
+    word_from = input("From: ")
+    word_to = input("To: ")
+    print(graph.search(word_from, word_to))
+
