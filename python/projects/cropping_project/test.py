26deddd7c6dd0a6d2dd902ab4eb2e26b26da3dfc
Jordan McQueen <jmcqueen@cardkingdom.com>
2016-08-16 15:37:07 -0700

    hacked together a last-minute request of employer -- 1 hour before end of last day. ;)

--- python/projects/cropping_project/test.py
+++
@@ -1,8 +0,0 @@
-good_dimensions = []
-height = 600
-while height < 1480:
-    if (298 * height) % 431 == 0:
-        width = 298 * height / 431
-        good_dimensions.append((width, height))
-    height += 1
-print(good_dimensions)
