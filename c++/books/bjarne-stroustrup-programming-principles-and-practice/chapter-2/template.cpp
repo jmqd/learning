ad6a6817295bbfe8763379dfb100708623d623f1
Jordan McQueen <dojo3@uw.edu>
2016-09-23 21:23:14 -0700

    adding template

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-2/template.cpp
@@ -0,0 +1,14 @@
+// A small template for the first few chapters of the book to save time.
+// It may be the case that some of these includes are unnecessary.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+inline void keep_open() {char ch; std::cin >> ch;}
+
+int main()  // C++ programs start by executing the main function
+{
+    // code here
+}
