e69c9067e01709f12cf747de9a8f331f9e46cb16
Jordan McQueen <dojo3@uw.edu>
2016-09-23 21:35:20 -0700

    first iteration of try this.

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-3/try-this/name_and_age.cpp
@@ -0,0 +1,22 @@
+// This file was constructed from a template designed for the first few
+// chapters of the book to save time. It may be the case that some of these
+// includes are unnecessary. That's okay.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+inline void keep_open() {char ch; std::cin >> ch;}
+
+int main()  // C++ programs start by executing the main function
+{
+    // vars
+    std::string first_name = "";
+    int age = 0;
+    std::cout << "Please enter your name.\n";
+    std::cin >> first_name;
+    std::cout << "Please enter your age.\n";
+    std::cin >> age;
+    std::cout << "Hello, " << first_name << " (age " << age << ")\n";
+}
