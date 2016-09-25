0869cf61ae653b8cacee1013c9d04d15d48ae3cc
Jordan McQueen <dojo3@uw.edu>
2016-09-24 19:30:41 -0700

    buggy code example

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-3/try-this/fix_these_bugs.cpp
@@ -0,0 +1,17 @@
+// This file was constructed from a template designed for the first few
+// chapters of the book to save time. It may be the case that some of these
+// includes are unnecessary. That's okay.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+inline void keep_open() {char ch; std::cin >> ch;}
+
+int Main()  // C++ programs start by executing the main function
+{
+    STRING s = "Goodbye cruel world!";
+    cOut << S << "\n";
+}
+
