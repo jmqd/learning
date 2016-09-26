935715bdfa6edb679ffb7557350f5cc96877e37c
Jordan McQueen <dojo3@uw.edu>
2016-09-25 17:01:42 -0700

    finishing drill from chapter 3.

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-3/drill_1.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-3/drill_1.cpp
@@ -17,36 +17,41 @@ int main()
     std::string last_seen = "";
     std::string activity = "";
     std::string mutual_friend = "";
-    char friend_sex = '0';
-    int friend_age = 0;
-    std::string author_name = "";
+    std::string friend_sex;
+    std::string friend_age = "";
+    std::string author_name = "Jordan";
+
+    int friend_age_int = 0;
 
     // user input
-    std::cout << "Recepient name: ";
-    std::cin >> name;
-    std::cout << "When did you last see them? ";
-    std::cin >> last_seen;
+    std::cout << "Recepient name\n";
+    std::getline(std::cin, name);
+    std::cout << "When did you last see them?\n";
+    std::getline(std::cin, last_seen);
     std::cout << "Beginning with a verb, desribe an"
-        "activity you'd like to suggest: ";
-    std::cin >> activity;
-    std::cout << "Who is a mutual friend? > ";
-    std::cin >> mutual_friend;
-    std::cout << mutual_friend << " is 'M' or 'F'?";
-    std::cin >> friend_sex;
-    std::cout << mutual_friend << " is how old (in years): ";
-    std::cin >> friend_age;
-    std::cout << "What is your name? ";
-    std::cin >> author_name;
+        "activity you'd like to suggest:\n";
+    std::getline(std::cin, activity);
+    std::cout << "Who is a mutual friend?\n";
+    std::getline(std::cin, mutual_friend);
+    std::cout << mutual_friend << " is 'M' or 'F'?\n";
+    std::getline(std::cin, friend_sex);
+    std::cout << mutual_friend << " is how old (in years):\n";
+    std::getline(std::cin, friend_age);
+    std::cout << "What is your name?\n";
+    std::getline(std::cin, author_name, '\n');
+    std::cout << "\n\n";
 
-    // processing, if needed:
-    std::string friend_pronoun = (friend_sex == 'M') ? "he" : "she";
+    // processing
+    std::string friend_pronoun = (friend_sex == "M") ? "he" : "she";
+    friend_age_int = std::stoi(friend_age, nullptr, 10);
 
     std::string letter_contents = "Dear " + name + ",\n\n"
         "It has been " + last_seen + " since I've seen you.\n"
         "I am looking forward to " + activity + " with you in the near future."
         "\nHave you seen " + mutual_friend + " lately? I hope"
-        " " + friend_pronoun + " is doing well\n. I heard " + friend_pronoun +
-        " is turning " + std::to_string(friend_age + 1) + " next year...\n\n\n"
-        "In any event, yours truly,\n\n" + author_name;
+        " " + friend_pronoun + " is doing well.\nI heard " + friend_pronoun +
+        " is turning " + std::to_string(friend_age_int + 1)
+        + " next year...\n\n\n"
+        "In any event, yours truly,\n\n" + author_name + '\n';
     std::cout << letter_contents;
 }
