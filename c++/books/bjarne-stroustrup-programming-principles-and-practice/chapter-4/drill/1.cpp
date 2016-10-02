c7f938ea7b7a588e63f51aca6e28abd6cfe898cd
Jordan McQueen <dojo3@uw.edu>
2016-10-02 15:34:50 -0700

    drill 2 and 3

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/drill/1.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/drill/1.cpp
@@ -8,6 +8,8 @@ int main()
 {
     int a = 0;
     int b = 0;
+    std::vector<int> nums;
+
     while (std::cin >> a >> b)
     {
         if (a == '|' || b == '|')
@@ -16,7 +18,24 @@ int main()
         }
         else
         {
-            std::cout << a << b;
+            // 2nd drill: larger and smaller
+            nums.push_back(a);
+            nums.push_back(b);
+            std::sort(nums.begin(), nums.end());
+            if (nums[0] != nums[1])
+            {
+                for (int i = 0; i < nums.size(); ++i)
+                {
+                    std::string comp = (i == 0) ? "smaller" : "larger";
+                    std::cout << nums[i] << " is the " << comp << " number\n"; 
+                }
+            }
+
+            if (nums[0] == nums[1])
+            {
+                std::cout << "the numbers are equal.\n";
+            }
         }
+        nums.clear();
     }
 }
