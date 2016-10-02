8eb71a5ec23e1beed2852e5bdacc59f4b3288245
Jordan McQueen <dojo3@uw.edu>
2016-10-01 17:35:37 -0700

    try this exercise: currency conversion

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/try-this/currency_conversion.cpp
@@ -0,0 +1,43 @@
+// This file was constructed from a template designed for the first few
+// chapters of the book to save time. It may be the case that some of these
+// includes are unnecessary. That's okay.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    char curr_type = '\0';
+    double amount;
+    constexpr double pounds_usd = 1.3;
+    constexpr double yen_usd = 0.0099;
+    constexpr double euro_usd = 1.12;
+    double ratio = 0.0;
+    double answer = 0.0;
+
+    std::cout << "\nEnter the type of currency you have.\n\n"
+        "pounds = p\nyen = y\neuros = e\n"
+        "\n> ";
+    std::cin >> curr_type;
+    std::cout << "\nEnter the amount you have.\n> ";
+    std::cin >> amount;
+    
+    switch (curr_type)
+    {
+        case 'p':
+            ratio = pounds_usd;
+            break;
+        case 'y':
+            ratio = yen_usd;
+            break;
+        case 'e':
+            ratio = euro_usd;
+            break;
+    }
+    answer = ratio * amount;
+    std::cout << "In USD, " << amount << curr_type << " is $"
+        << std::to_string(answer) << "\n";
+}
