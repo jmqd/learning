d57d3c24ac0adcec7f6e540420eb6ac3446eedab
Jordan McQueen <dojo3@uw.edu>
2016-10-01 17:46:51 -0700

    try this exercise

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/try-this/table_of_squares_seq.cpp
@@ -0,0 +1,17 @@
+// This file was constructed from a template designed for the first few
+// chapters of the book to save time. It may be the case that some of these
+// includes are unnecessary. That's okay.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    for (int i = 0; i < 100; ++i)
+    {
+        std::cout << "\t" << i << "\t" << i * i << "\n";
+    }
+}
