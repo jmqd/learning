a855ccc6a69a59fe9175a1ffc812aa34437eacb9
Jordan McQueen <dojo3@uw.edu>
2016-10-04 00:09:45 -0700

    skeleton for 4th exercise

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/4.cpp
@@ -0,0 +1,14 @@
+// Write a program to play a numbers guessing game.
+// Program should be able to figure out the number I'm guessing
+// between 1 and 100 within 7 questions.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    // code here
+}
