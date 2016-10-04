5f6a243ce3dbf186036149ffbef1e35bb35d22dc
Jordan McQueen <dojo3@uw.edu>
2016-10-04 00:14:16 -0700

    skeleton for 7

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/7.cpp
@@ -0,0 +1,13 @@
+// Modify the calculator from exercise 5 to accept (just) single-digit
+// numbers written as either digits or spelled out.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    // code here
+}
