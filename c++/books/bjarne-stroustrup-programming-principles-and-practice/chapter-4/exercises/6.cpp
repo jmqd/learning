ccda01a3fcce6a9aaf327440a1e168ce42e77b39
Jordan McQueen <dojo3@uw.edu>
2016-10-04 00:13:13 -0700

    skeleton for 6th exercise

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/6.cpp
@@ -0,0 +1,14 @@
+// Make a vector holding the ten string values "zero", "one", etc.
+// Use that in a program that converts a digit to its corresponding
+// spelled out value, and vice-a-versa.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    // code here
+}
