bf5860cb5dfd412058668e4fcc923f718886e83d
Jordan McQueen <dojo3@uw.edu>
2016-10-04 00:11:36 -0700

    skeleton for 5th exercise

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/5.cpp
@@ -0,0 +1,17 @@
+// Write a simple calculator, that can handle two doubles and an operand.
+//
+// e.g.
+//
+// 1 5 + => cout << "The sum of 1 and 5 is 6"
+// 6 2 * => cout << "The multiplication of 6 and 2 is 12"
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+int main()
+{
+    // code here
+}
