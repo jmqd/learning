21ad2f8157eec32d0efb1ef084cb6225519789f3
Jordan McQueen <dojo3@uw.edu>
2016-10-16 23:31:28 -0700

    digit to string and reverse converter

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/6.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/6.cpp
@@ -10,5 +10,37 @@
 
 int main()
 {
-    // code here
+    // initializations
+    char direction;
+    std::vector<std::string> nums { "zero", "one", "two", "three", "four",
+                                    "five", "six", "seven", "eight", "nine" };
+
+    // controller
+    std::cout << "Do you have a digit (d), or a spelled-out-value (s)? > ";
+    std::cin >> direction;
+    std::cout << "\nOkay, please enter the value to convert. > ";
+    if (direction == 's')
+    {
+        std::string spelled_out_num = "";
+        std::cin >> spelled_out_num;
+        int digit = std::find(nums.begin(),
+                nums.end(), spelled_out_num) - nums.begin();
+        std::cout << '\n'
+            << spelled_out_num
+            << " corresponds to digit "
+            << digit << '\n';
+    }
+    else if (direction == 'd')
+    {
+        int digit = 0;
+        std::cin >> digit;
+        std::string spelled_out_num = nums[digit];
+        std::cout << '\n' << digit
+            << " corresponds to spelled out name  '"
+            << spelled_out_num << "'\n";
+    }
+    else
+    {
+        std::cout << "\nI don't understand that type of input.\n";
+    }
 }
