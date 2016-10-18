0214389ae0cd160a254132782d51f88cf6374e92
Jordan McQueen <dojo3@uw.edu>
2016-10-18 00:22:52 -0700

    initial commit: emperor, rice, chess

---
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/8.cpp
@@ -0,0 +1,21 @@
+// There is an old story that the emperor wanted to thak the inventor
+// of the game of chess and asked the inventor to name his reward.
+// The inventor asked for one grain of rice of the first square,
+// 2 for the second, 4 for the third, and so on, doubling for each
+// of the 64 squares. That may sound modest, but there wasn't enough
+// rice in the empire to satisfy the request! Write a program to calculate
+// how many squares are required to give the inventor at least 1000 grains
+// of rice, at least 1,000,000 grains, at least 1,000,000,000 grains.
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<regex>
+#include<map>
+
+int main()
+{
+
+}
