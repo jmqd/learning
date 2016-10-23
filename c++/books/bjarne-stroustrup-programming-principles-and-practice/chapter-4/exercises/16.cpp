8d2ed90f4beef4c8b6ea6f89253923c614fa8c2e
Jordan McQueen <dojo3@uw.edu>
2016-10-23 14:15:48 -0700

    mode of sequence

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/16.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/16.cpp
@@ -10,5 +10,38 @@
 
 int main()
 {
-
+    int input = 0;
+    std::vector<int> v;
+    
+    std::cout << "I'll get the mode of some integers for you. A sequence\n"
+        "with multiple modes will return the lexicographically smallest mode.";
+    std::cout << "\n=======================================================\n";
+    std::cout << "\nEnter space separated integers, terminated with 'q'.\n";
+    // using 'q' to terminate input
+    while (std::cin >> input && input != 'q')
+    {
+        v.push_back(input);
+    }
+    std::sort(v.begin(), v.end());
+    int current_count = 0;
+    int max_count = 0;
+    int mode = 0;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (i == 0 || v[i - 1] == v[i])
+        {
+            current_count++;
+            if (current_count > max_count)
+            {
+                max_count = current_count;
+                mode = i;
+            }
+        }
+        else
+        {
+            current_count = 1;
+        }
+    }
+    std::cout << "The mode for that collection of integers is: " << v[mode]
+        << '\n';
 }
