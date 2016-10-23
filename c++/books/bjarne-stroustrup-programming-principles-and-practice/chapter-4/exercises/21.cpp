b58052c3e6c397820c1d0f505456903179b95761
Jordan McQueen <dojo3@uw.edu>
2016-10-23 15:09:11 -0700

    names and ages; final exercise for chap 4

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/21.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/21.cpp
@@ -1,5 +1,5 @@
-// Modify the program from exercise 19 so that when you enter an integer,
-// the program will output all names with that age or "No persons of that age."
+// Modify program from previous exercise to allow querying
+// by age at the end.
 
 #include<iostream>
 #include<string>
@@ -11,5 +11,50 @@
 
 int main()
 {
+    std::vector<std::string> names;
+    std::vector<int> ages;
+    std::string name = "";
+    int age = 0;
 
+    std::cout << "\nBook of names and ages.\n";
+    std::cout << "===========================================================";
+    std::cout << "\nInput is terminated by entering END";
+    std::cout << "\nEnter some name age pairs, like so: Joe 5 Jordan 23 etc\n";
+    while (std::cin >> name && name != "END")
+    {
+        names.push_back(name);
+        std::cin >> age;
+        ages.push_back(age);
+    }
+
+    for (int i = 0; i < names.size(); ++i)
+    {
+        std::cout << names[i] << " is " << ages[i] << " years old.\n";
+    }
+
+    int query = 0;
+    std::cout << "\nEnter an age to find names w/ that age > ";
+    std::cin >> query;
+    std::vector<std::string> query_results;
+
+    for (int i = 0; i < ages.size(); ++i)
+    {
+        if (ages[i] == query)
+        {
+            query_results.push_back(names[i]);
+        }
+    }
+
+    if (query_results.size() == 0)
+    {
+        std::cout << "Sorry, I couldn't find " << query << " in the agebook.";
+        return 0;
+    }
+
+    std::cout << "Names with that age\n";
+    std::cout << "===========================================================";
+    for (std::string j: query_results)
+    {
+        std::cout << "\n" << j;
+    }
 }
