54b6fafa63221f90084464e6dc18947632f8521c
Jordan McQueen <dojo3@uw.edu>
2016-10-23 12:49:55 -0700

    sieve of eratosthenes

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/13.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/13.cpp
@@ -11,5 +11,32 @@
 
 int main()
 {
+    // initializations
+    int v_length = 101;
+    std::vector<bool> number_grid(v_length, true);
 
+    // ugly, I know...
+    number_grid[0] = false;
+    number_grid[1] = false;
+
+    // sieve of erathosthenes
+    for (int i = 2; i < v_length; ++i)
+    {
+        if (number_grid[i])
+        {
+            for (int j = 2 * i; j < v_length; j += i)
+            {
+                number_grid[j] = false;
+            }
+        }
+    }
+
+    // printing output
+    for (int i = 1; i < v_length; ++i)
+    {
+        if (number_grid[i])
+        {
+            std::cout << i << '\n';
+        }
+    }
 }
