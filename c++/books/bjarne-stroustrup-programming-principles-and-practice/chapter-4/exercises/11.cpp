22e73368a485aabed23542cd2030588309ddea55
Jordan McQueen <dojo3@uw.edu>
2016-10-23 01:21:20 -0700

    naive prime generator

--- c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/11.cpp
+++ c++/books/bjarne-stroustrup-programming-principles-and-practice/chapter-4/exercises/11.cpp
@@ -8,7 +8,47 @@
 #include<regex>
 #include<map>
 
+bool is_prime(int num)
+{
+    if (num == 1)
+    {
+        return false;
+    }
+
+    if (num == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i < std::sqrt(num); ++i)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
+    // going to build a naive approach because I noticed that
+    // further exercises will apptach a more efficient algorithm
 
+    for (int i = 1; i < 101; ++i)
+    {
+        std::string result = "";
+        bool prime = is_prime(i);
+
+        if (prime)
+        {
+           result = "prime"; 
+        }
+        else
+        {
+            result = "not prime";
+        }
+
+        std::cout << i << " is " << result << '\n';
+    }
 }
