5cf29026c4d9116af1f0ce61b16704bc7c56e7b1
Jordan McQueen <dojo3@uw.edu>
2016-11-27 16:23:14 -0800

    expository notes on insertion sort

---
+++ algorithms/python/insertion-sort/insertion_sort.py
@@ -0,0 +1,29 @@
+'''
+Reference:
+    Introduction to Algorihths, CLRS
+
+Notes:
+    Expository notes on the Insertion Sort algorithm, a sorting method
+    I've never looked at.
+'''
+
+def sort(arr):
+    '''Given an unsorted array, sort it.'''
+    for i, elem in enumerate(arr):
+        # for each iteration, everything up to i is in order.
+        # we need only find the correct spot for arr[i] (elem)
+        # in the subarray [0..arr[i - 1]]
+
+        # start in the position just before i when looking for the
+        # correct insertion index, and work backwards
+        insertion_index = i - 1
+
+        while insertion_index >= 0 and arr[insertion_index] > elem:
+            # iterate backwards until we find an index where the current
+            # element is greater, or if it is smaller than all elements,
+            # we insert it at i = 0.
+            arr[insertion_index + 1] = arr[insertion_index]
+            insertion_index -= 1
+
+        # insert the element
+        arr[insertion_index + 1] = elem
