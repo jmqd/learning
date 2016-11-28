dc13b441f28bcf7e7c47528655c275fdbc164840
Jordan McQueen <dojo3@uw.edu>
2016-11-28 06:44:35 -0800

    bugfix w/ time conversion

--- coding-challenges/python/hackerrank/time-conversion/solution.py
+++ coding-challenges/python/hackerrank/time-conversion/solution.py
@@ -4,8 +4,8 @@ import re
 def to_military(time):
     ampm_regex = re.compile(r'(^\d{2})(:\d{2}:\d{2})([A-Za-z]{2})')
     hours, mins_and_secs, time_period = ampm_regex.findall(time)[0]
-    military_time = (hours.zfill(2) if time_period == 'AM' else str((int(hours)
-        + 12) % 24).zfill(2)) + mins_and_secs
+    military_time = str(int(hours) % 12 + (12 if time_period == 'PM' else 0)
+            ).zfill(2) + mins_and_secs
     return military_time
 
 def hackerrank_solve():
@@ -20,11 +20,13 @@ def test():
         '11:32:32AM',
         '04:41:11PM',
         '12:51:21PM',
+        '12:51:21AM',
+        '11:51:21PM',
         '07:21:41PM',
         '01:41:12AM'
         ]
     for case in cases:
-        print(to_military(case))
+        print(case, ' --> ', to_military(case))
 
 if __name__ == '__main__':
     main()
