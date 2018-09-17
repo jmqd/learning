2481a17ed6e846353bcc9d9a7247a7f3297fa76b
Jordan McQueen <jordan@whoami.sh>
2018-09-17 01:53:29 -0700

    complete integer to eng word

--- coding-challenges/sites/lc/integer-to-english-words/solution.py
+++ coding-challenges/sites/lc/integer-to-english-words/solution.py
@@ -1,10 +1,9 @@
-from math import log, floor
-
 UNDER_TWENTY = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
     "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
     "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
 TENS = ["Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-MULTIPLIERS = ["Hundred", "Thousand", "Million", "Billion", "Trillion"]
+MULTIPLIERS = ["Thousand", "Million", "Billion", "Trillion"]
+MULTIPLIER_PARTITION_SIZE = 3
 
 class Solution:
     def numberToWords(self, num):
@@ -12,36 +11,48 @@ class Solution:
         :type num: int
         :rtype: str
         """
+        if num == 0:
+            return "Zero"
+
         words = []
-        modulate_number(num, words)
+        digits = count_digits(num)
+        pivot = digits % MULTIPLIER_PARTITION_SIZE
+        position = digits - pivot
+        partitioned_num = num // 10 ** position
+        while position >= 0:
+            if partitioned_num:
+                modulate_number(partitioned_num, words)
+                if position >= 3:
+                    words.append(MULTIPLIERS[(position // 3) - 1])
+            num -= partitioned_num * 10 ** position
+            position -= MULTIPLIER_PARTITION_SIZE
+            partitioned_num = num // 10 ** position
+
         print(words)
 
 def modulate_number(num, words):
-        modulator = 10
-        while num > 999:
-            remainder = num % modulator
-            num -= remainder
-            modulator *= 10
-            print(remainder)
+    if num < 20:
+        words.append(UNDER_TWENTY[num])
+    elif num < 100:
+        words.append(TENS[(num // 10) - 2])
+        words.append(UNDER_TWENTY[num % 10])
+    elif num < 1000:
+        words.append(UNDER_TWENTY[num // 100])
+        words.append("Hundred")
+        modulate_number(num % 100, words)
 
-            if not remainder:
-                continue
-
-            order = floor(log(remainder + 1, 10))
-            print("Order of remainder is: {}".format(order))
-
-            if remainder < 20:
-                words.append(UNDER_TWENTY[remainder])
-            elif order == 1:
-                words.append(TENS[(remainder // 10) - 2])
-            elif order == 2:
-                print("ALERT", order, remainder)
-            elif order > 3:
-                print("remainder // modulator = ", remainder // 1000)
-                words.append(MULTIPLIERS[order - 4])
+def count_digits(num):
+    count = 0
+    while num:
+        num //= 10
+        count += 1
+    return count
 
 def test():
     s = Solution()
     s.numberToWords(1210543)
+    s.numberToWords(12345)
+    s.numberToWords(1234567891)
+    s.numberToWords(123)
 
 test()
